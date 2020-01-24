d73cf6c5c2ce0eedb2ae7d488d2b0ac3757f6c80
SpiralP <spiral.palmer@gmail.com>
2020-01-24 15:03:36 -0800

    fix webpack-dev-server dir, recommend prettier extension

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,47 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
-    globalObject: "self",
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.ttf$/,
-        use: ["file-loader"],
-      },
+    mode: "production",
+    entry: "./src/index.ts",
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    output: {
+        globalObject: "self",
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.ttf$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new MonacoWebpackPlugin(),
+        // new webpack.optimize.LimitChunkCountPlugin({
+        //   maxChunks: 1, // disable creating additional chunks
+        // }),
     ],
-  },
-  plugins: [
-    new MonacoWebpackPlugin(),
-    // new webpack.optimize.LimitChunkCountPlugin({
-    //   maxChunks: 1, // disable creating additional chunks
-    // }),
-  ],
 
-  // having minimize enabled gives weird Â's for blank lines??
-  optimization: {
-    minimize: false,
-  },
+    // having minimize enabled gives weird Â's for blank lines??
+    optimization: {
+        minimize: false,
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 9000,
+    },
 };
